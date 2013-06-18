75ad40997cb3a88b717f0ec4ad3bf7a8000651bf
yellia <yellia025@gmail.com>
2013-06-18 16:49:12 +0800

    init

---
+++ netlib/basic_io_server.h
@@ -0,0 +1,13 @@
+#ifndef BASIC_IO_SERVER_H
+#define BASIC_IO_SERVER_H
+
+namespace netlib
+{
+	template<typename Message>
+	class basic_io_server
+	{
+	
+	};
+}
+
+#endif
---
+++ netlib/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
